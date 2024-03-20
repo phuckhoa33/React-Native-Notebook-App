09130d8a2d4032558f68612fc54279285bf84785
Admin <phuckhoa33>
2024-03-20 13:58:44 +0700

    seperate pages and componets

---
+++ components/AppNavigator.js
@@ -0,0 +1,9 @@
+import { NavigationContainer } from '@react-navigation/native';
+
+export const AppNavigator = ({navRef}) => {
+    return (
+        <NavigationContainer ref={navRef}>
+
+        </NavigationContainer>
+    )
+}
